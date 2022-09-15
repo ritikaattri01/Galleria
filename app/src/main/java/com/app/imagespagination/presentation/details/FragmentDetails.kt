5332027350a69b3057378a35bb5487957b13add9
Ritika Digra <ritikaattri8@gmail.com>
2022-09-15 10:19:58 +0530

    details page and code cleanup

--- app/src/main/java/com/app/imagespagination/presentation/details/FragmentDetails.kt
+++ app/src/main/java/com/app/imagespagination/presentation/details/FragmentDetails.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.app.imagespagination.R
 import com.app.imagespagination.databinding.FragmentDetailsBinding
 import com.bumptech.glide.Glide
 
-class FragmentDetails : Fragment() {
+class FragmentDetails : DialogFragment() {
 
     private val args: FragmentDetailsArgs by navArgs()
 
